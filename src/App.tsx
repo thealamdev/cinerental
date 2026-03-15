301ee4788e723f3cb29ff9264aa4fec0ff0d78bc
Md. Shah Alam <shahalam141072@gmail.com>
2026-03-15 09:59:54 +0600

    :pencil2: => CineRental Project installed

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+
+function App() {
+
+  return (
+    <div className="dark:bg-body bg-white font-[Sora] dark:text-white text-dark">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,33 @@
+import ring from './assets/ring.svg';
+import sum from './assets/icons/sun.svg';
+import ShopingCard from './assets/shopping-cart.svg';
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="container flex items-center justify-between space-x-10 py-6">
+                <a href="index.html">
+                    <img src="./assets/logo.svg" width="139" height="26" alt="" />
+                </a>
+
+                <ul className="flex items-center space-x-5">
+                    <li>
+                        <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                            <img src={ring} width="24" height="24" alt="ring" />
+                        </a>
+                    </li>
+                    <li>
+                        <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                            <img src={sum} width="24" height="24" alt="sun" />
+                        </a>
+                    </li>
+                    <li>
+                        <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                            <img src={ShopingCard} width="24" height="24" alt="" />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
